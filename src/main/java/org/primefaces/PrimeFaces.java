65348a2a091c60b2e9b17cb24ce1a19ef2c35e7e
Alberto Fernandez <infjaf@gmail.com>
2020-04-29 11:59:36 +0200

    improve robustness

--- src/main/java/org/primefaces/PrimeFaces.java
+++ src/main/java/org/primefaces/PrimeFaces.java
@@ -89,6 +89,14 @@ public class PrimeFaces {
     protected PrimeRequestContext getRequestContext() {
         return PrimeRequestContext.getCurrentInstance();
     }
+    /**
+     * Checks if the current request is a Faces request.
+     *
+     * @return <code>true</code> if the current request is a Faces request.
+     */
+    public boolean isFacesRequest() {
+        return getFacesContext() != null;
+    }
 
     /**
      * Shortcut for {@link PartialViewContext#isAjaxRequest()}.
@@ -96,7 +104,11 @@ public class PrimeFaces {
      * @return <code>true</code> if the current request is a AJAX request.
      */
     public boolean isAjaxRequest() {
-        return getFacesContext().getPartialViewContext().isAjaxRequest();
+        FacesContext context = getFacesContext();
+        if (context == null) {
+            return false;
+        }
+        return context.getPartialViewContext().isAjaxRequest();
     }
 
     /**
