ab0850fae8d136f79ec408bbaa18ac50845ae9df
Alberto Fernandez <infjaf@gmail.com>
2021-03-14 14:34:37 +0100

    Message validation using tooltip

--- src/main/resources/META-INF/resources/primefaces/messages/messages.js
+++ src/main/resources/META-INF/resources/primefaces/messages/messages.js
@@ -20,7 +20,7 @@ PrimeFaces.widget.Message = PrimeFaces.widget.BaseWidget.extend({
     init: function(cfg) {
         this._super(cfg);
         
-        var text = this.jq.children('.ui-message-error-detail').text();
+        var text = this.jq.find('.ui-message-error-detail').text();
         
         if(text) {
            var target = $(PrimeFaces.escapeClientId(this.cfg.target));
